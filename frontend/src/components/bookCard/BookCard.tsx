c400d0f431705ea0e6b206a34cd1074c6ee2ff4c
Simon Staß <simonsta95@gmail.com>
2024-08-14 14:50:45 +0200

    add Component to view all books

---
+++ frontend/src/components/bookCard/BookCard.tsx
@@ -0,0 +1,19 @@
+import "./BookCard.css"
+
+import {Book} from "../../types/types.ts";
+
+type BookCardProps = {
+    book: Book
+}
+
+export default function BookCard({book}: BookCardProps) {
+    return (
+
+        <li>
+            <p>Id: {book.id}</p>
+            <p>Title: {book.title}</p>
+            <p>Author:{book.author}</p>
+        </li>
+
+    );
+}
