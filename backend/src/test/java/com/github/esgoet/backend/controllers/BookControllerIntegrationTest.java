ec8a832d8f556201741375ed6c42e762dfc4c0eb
esgoet <toddesgo@gmail.com>
2024-08-14 16:20:31 +0200

    Add unit and integration test for backend

--- backend/src/test/java/com/github/esgoet/backend/controllers/BookControllerIntegrationTest.java
+++ backend/src/test/java/com/github/esgoet/backend/controllers/BookControllerIntegrationTest.java
@@ -1,14 +1,17 @@
 package com.github.esgoet.backend.controllers;
 
+import com.github.esgoet.backend.models.Book;
+import com.github.esgoet.backend.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,14 +19,50 @@ class BookControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    BookRepository bookRepository;
 
     @Test
     public void getAllBooks_Test_When_DbEmpty_Then_returnEmptyArray() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("[]"));
+                .andExpect(content().json("[]"));
 
+    }
+
+    @DirtiesContext
+    @Test
+    void getBook_Test_whenIdExists() throws Exception {
+        //GIVEN
+        bookRepository.save(new Book("1","George Orwell", "1984"));
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books/1"))
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                          "id": "1",
+                          "author": "George Orwell",
+                          "title": "1984"
+                        }
+                        """));
+    }
+
+    @DirtiesContext
+    @Test
+    void getBook_Test_whenIdDoesNotExists() throws Exception {
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books/1"))
+                //THEN
+                .andExpect(status().isNotFound())
+                .andExpect(content().json("""
+                        {
+                          "message":"No book found with id: 1",
+                          "statusCode":404
+                        }
+                        """))
+                .andExpect(jsonPath("$.timestamp").exists());
     }
 
 }
