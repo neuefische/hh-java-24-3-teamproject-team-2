6ddbd13f3de5519739b52978990d3f7cad77e29d
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-14 16:23:33 +0200

    Write unit & integration tests for POST method (add a new book)

--- backend/src/test/java/com/github/esgoet/backend/controllers/BookControllerIntegrationTest.java
+++ backend/src/test/java/com/github/esgoet/backend/controllers/BookControllerIntegrationTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,7 +25,33 @@ class BookControllerIntegrationTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("[]"));
+                .andExpect(content().json("[]"));
+
+    }
+
+    @Test
+    @DirtiesContext
+    public void addABookTest_whenNewTodoExists_thenReturnNewTodo() throws Exception {
+        // GIVEN
+
+        // WHEN
+        mockMvc.perform(post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                     {
+                         "author": "Tolstoy",
+                         "title": "War and Peace",
+                         "genre": "HISTORY",
+                         "publicationDate": "1869-01-01"
+                     }
+                """))
+                // THEN
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.author").value("Tolstoy"))
+                .andExpect(jsonPath("$.title").value("War and Peace"))
+                .andExpect(jsonPath("$.genre").value("HISTORY"))
+                .andExpect(jsonPath("$.publicationDate").value("1869-01-01"));
 
     }
 
--- backend/src/test/java/com/github/esgoet/backend/services/BookServiceUnitTest.java
+++ backend/src/test/java/com/github/esgoet/backend/services/BookServiceUnitTest.java
@@ -1,10 +1,13 @@
 package com.github.esgoet.backend.services;
 
+import com.github.esgoet.backend.dto.NewBookDto;
 import com.github.esgoet.backend.models.Book;
+import com.github.esgoet.backend.models.Genre;
 import com.github.esgoet.backend.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +18,20 @@ import static org.mockito.Mockito.*;
 class BookServiceUnitTest {
 
     private final BookRepository bookRepo = mock(BookRepository.class);
-    private final BookService bookService = new BookService(bookRepo);
+    private final IdService idService = mock(IdService.class);
+    private final BookService bookService = new BookService(bookRepo, idService);
+    private final LocalDate localDate = LocalDate.parse("2024-08-14");
 
     @Test
     void getAllBooks_Test() {
         List<Book> allBooks = List.of(
-                new Book("1", "Simon", "Java for Dummies"),
-                new Book("2","Florian", "Java not for Dummies")
+                new Book("1", "Simon", "Java for Dummies", Genre.SCIENCE, localDate),
+                new Book("2","Florian", "Java not for Dummies", Genre.SCIENCE, localDate)
         );
 
         List<Book> expectedBooks = List.of(
-                new Book("1", "Simon", "Java for Dummies"),
-                new Book("2","Florian", "Java not for Dummies")
+                new Book("1", "Simon", "Java for Dummies", Genre.SCIENCE, localDate),
+                new Book("2","Florian", "Java not for Dummies", Genre.SCIENCE, localDate)
         );
 
         when(bookRepo.findAll()).thenReturn(allBooks);
@@ -42,5 +47,21 @@ class BookServiceUnitTest {
         List<Book> actualBooks = bookService.getAllBooks();
 
         assertEquals(expectedBooks, actualBooks);
+    }
+
+    @Test
+    void addABookTest_whenNewBookAsInput_thenReturnNewBook() {
+        // GIVEN
+        NewBookDto newBookDto = new NewBookDto("J. K. Rowling", "Harry Potter", Genre.FANTASY, localDate);
+        Book bookToSave = new Book(idService.randomId(), newBookDto.author(), newBookDto.title(), newBookDto.genre(), newBookDto.publicationDate());
+        when(bookRepo.save(bookToSave)).thenReturn(bookToSave);
+
+        // WHEN
+        Book actual = bookService.saveNewABook(newBookDto);
+
+        // THEN
+        Book expected = bookToSave;
+        verify(bookRepo).save(bookToSave);
+        assertEquals(expected, actual);
     }
 }
