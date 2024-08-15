d454d49f8fdc9d08eb9354005f6efb960c65b922
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-15 11:39:27 +0200

    Send a new book data from the form

--- frontend/src/pages/BookGalleryPage/components/addBookButton/AddBookForm.tsx
+++ frontend/src/pages/BookGalleryPage/components/addBookButton/AddBookForm.tsx
@@ -1,5 +1,6 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import {NewBook} from "../../../../types/types.ts";
+import axios from "axios";
 
 export default function AddBookForm() {
 
@@ -12,31 +13,42 @@ export default function AddBookForm() {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        console.log(event);
         console.log(book);
+        axios.post("/api/books", {
+            title: book.title,
+            author: book.author,
+            genre: book.genre,
+            publicationDate: book.publicationDate
+        })
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <label>Add a new book here</label>
+            <label>Title</label>
             <input
                 type={"text"}
                 name={"title"}
                 value={book.title}
                 onChange={handleChange}
             />
+            <label>Author</label>
             <input
                 type={"text"}
                 name={"author"}
                 value={book.author}
                 onChange={handleChange}
             />
+            <label>Genre</label>
             <input
                 type={"text"}
                 name={"genre"}
                 value={book.genre}
                 onChange={handleChange}
             />
+            <label>Publication Date</label>
             <input
                 type={"date"}
                 name={"publicationDate"}
