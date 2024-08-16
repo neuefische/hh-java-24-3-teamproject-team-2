3a10a8d9f66bfb5de7095033deaf557482ebf267
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-16 15:27:44 +0200

    Add temporary styles to the modal component

--- frontend/src/components/confirmationModal/ConfirmationModal.tsx
+++ frontend/src/components/confirmationModal/ConfirmationModal.tsx
@@ -1,3 +1,5 @@
+import "./ConfirmationModal.css";
+import DeleteIcon from "../../assets/delete-svgrepo-com.svg";
 
 type ModalProps = {
     handleClose: () => void,
@@ -8,12 +10,18 @@ type ModalProps = {
 export default function ConfirmationModal ({handleClose, handleDeleteConfirm, itemIdToBeDeleted}: ModalProps) {
 
     return (
-        <div className="modal">
-            <div className="modal-body">
-                <p>This is a modal component.</p>
+        <div className={"modal-backdrop"}>
+            <div className={"modal"}>
+                <div className={"modal-body"}>
+                    <img className={"delete-icon"} src={DeleteIcon} alt="delete icon"/>
+                    <h2 className={"modal-body-title"}>Delete Book</h2>
+                    <h4 className={"modal-body-message"}>Are you sure you want to delete this book?</h4>
+                </div>
+                <div className={"modal-buttons"}>
+                    <button className={"close-btn"} onClick={handleClose}>Close</button>
+                    <button className={"delete-btn"} onClick={() => handleDeleteConfirm(itemIdToBeDeleted)}>Delete</button>
+                </div>
             </div>
-            <button className="delete-btn" onClick={() => handleDeleteConfirm(itemIdToBeDeleted)}>Delete</button>
-            <button className="close-btn" onClick={handleClose}>Close</button>
         </div>
     )
 }
