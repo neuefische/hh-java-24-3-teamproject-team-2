d0324b2be0b449f6c33cc70d30567b0a88dfbf40
Marcel Herr <mrclhrr@gmail.com>
2024-08-16 14:14:24 +0200

    add nav tag

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -4,10 +4,12 @@ import {Link} from "react-router-dom";
 export default function Navigation() {
     return (
         <>
-            <ul>
-                <li><Link to={"/books"}>All Books</Link></li>
-                <li><Link to={"/books/add"}>Add Book</Link></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><Link to={"/books"}>All Books</Link></li>
+                    <li><Link to={"/books/add"}>Add Book</Link></li>
+                </ul>
+            </nav>
         </>
     )
 }
