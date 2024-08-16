5c620e89ffe375f6e3dff3d7455f575b8d2fef9e
Simon Staß <simonsta95@gmail.com>
2024-08-16 14:59:40 +0200

    fixed 1 sonarLint error

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -3,13 +3,11 @@ import {Link} from "react-router-dom";
 
 export default function Navigation() {
     return (
-        <>
             <nav>
                 <ul>
                     <li><Link to={"/books"}>All Books</Link></li>
                     <li><Link to={"/books/add"}>Add Book</Link></li>
                 </ul>
             </nav>
-        </>
     )
 }
