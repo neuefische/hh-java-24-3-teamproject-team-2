b3219895ce3532b0765e4e4ad174e6324e1bd790
esgoet <toddesgo@gmail.com>
2024-08-16 16:22:27 +0200

    Add BookForm to replace form in AddBookForm and EditForm

---
+++ frontend/src/components/bookForm/BookForm.tsx
@@ -0,0 +1,103 @@
+import {Book, NewBook} from "../../types/types.ts";
+import {ChangeEvent, FormEvent} from "react";
+import "./BookForm.css";
+
+type BookFormProps = {
+    book: Book | NewBook,
+    setBook: (book: Book | NewBook) => void,
+    handleSubmit: (event: FormEvent<HTMLFormElement>) => void,
+    action: string,
+    editable: boolean
+}
+
+/* not sure whether to use this, then genres would be of type Genre
+type Genre = "NONE" | "FICTION" | "MYSTERY" | "THRILLER" |
+    "FANTASY" | "SCIENCE" | "NON_FICTION" | "HISTORY" |
+    "NOVEL" | "HISTORICAL_FICTION" | "SCIENCE_FICTION" |
+    "ROMANCE" | "YOUNG_ADULT" | "ADVENTURE" | "HORROR";*/
+
+export default function BookForm({book, setBook, handleSubmit, action, editable}: Readonly<BookFormProps>) {
+    const genres : string[] = ["NONE", "FICTION", "MYSTERY", "THRILLER",
+    "FANTASY", "SCIENCE", "NON_FICTION", "HISTORY", "NOVEL", "HISTORICAL_FICTION", "SCIENCE_FICTION",
+    "ROMANCE", "YOUNG_ADULT", "ADVENTURE", "HORROR"];
+
+    function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | ChangeEvent<HTMLSelectElement>): void {
+        setBook({...book, [event.target.name]: event.target.value})
+    }
+
+    function formatGenre(genre :string): string {
+        return genre.split('_').map((letter) => {
+            return letter.charAt(0).toUpperCase() + letter.slice(1).toLowerCase();
+        }).join(genre != "NON_FICTION" ? " " : "-");
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className={"book-form"}>
+            <label className={"book-label align-right"} htmlFor={"title"}>Title</label>
+            <input
+                type={"text"}
+                name={"title"}
+                value={book.title}
+                onChange={handleChange}
+                required={true}
+                disabled={!editable}
+            />
+            <label className={"book-label align-right"} htmlFor={"author"}>Author</label>
+            <input
+                type={"text"}
+                name={"author"}
+                value={book.author}
+                onChange={handleChange}
+                required={true}
+                disabled={!editable}
+            />
+            <label className={"book-label align-right"} htmlFor={"description"}>Description</label>
+            <textarea rows={5} cols={30}
+                      name="description"
+                      value={book.description}
+                      onChange={handleChange}
+                      required={true}
+                      disabled={!editable}
+            />
+            <label className={"book-label align-right"} htmlFor={"genre"}>Genre</label>
+            <select required={true} value={book.genre} onChange={handleChange} name={"genre"}
+                    disabled={!editable}>
+                {genres.map((genre) => (
+                    <option key={genre} value={genre}>
+                        {formatGenre(genre)}
+                    </option>
+                ))}
+            </select>
+            <label htmlFor={"isbn"} className={"book-label align-right"}>ISBN</label>
+            <input
+                type="text"
+                name="isbn"
+                value={book.isbn}
+                onChange={handleChange}
+                required={true}
+                disabled={!editable}
+            />
+            <label htmlFor={"cover"} className={"book-label align-right"}>Cover</label>
+            <input
+                type="text"
+                name="cover"
+                value={book.cover}
+                onChange={handleChange}
+                required={true}
+                disabled={!editable}
+            />
+            <label htmlFor={"publicationDate"} className={"book-label align-right"}>Publication Date</label>
+            <input
+                type={"date"}
+                name={"publicationDate"}
+                value={book.publicationDate}
+                onChange={handleChange}
+                required={true}
+                disabled={!editable}
+            />
+            {editable && <button type={"submit"}>{action}</button>}
+        </form>
+
+    )
+
+}
