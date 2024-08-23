43407420d4cd3b2f9def2c6a8c6eb92245b2d7ae
esgoet <toddesgo@gmail.com>
2024-08-23 15:48:51 +0200

    Add mock user to other integration test

--- backend/src/test/java/com/github/esgoet/backend/user/controllers/UserControllerIntegrationTest.java
+++ backend/src/test/java/com/github/esgoet/backend/user/controllers/UserControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ class UserControllerIntegrationTest {
     private final LocalDate goalDate = LocalDate.parse("2024-12-31");
 
     @Test
+    @WithMockUser
     void getUsersTest() throws Exception {
         mockMvc.perform(get("/api/users"))
                 .andExpect(status().isOk())
@@ -34,6 +36,7 @@ class UserControllerIntegrationTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser
     void getUserByIdTest_whenIdExists() throws Exception{
         //GIVEN
         userRepository.save(new User("1","esgoet", 6, goalDate, 0));
@@ -53,6 +56,7 @@ class UserControllerIntegrationTest {
     }
 
     @Test
+    @WithMockUser
     void getUserByIdTest_whenIdDoesNotExist() throws Exception {
         //WHEN
         mockMvc.perform(get("/api/users/1"))
@@ -69,6 +73,7 @@ class UserControllerIntegrationTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser
     void addUserTest() throws Exception {
         //WHEN
         mockMvc.perform(post("/api/users")
@@ -95,6 +100,7 @@ class UserControllerIntegrationTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser
     void updateUserTest() throws Exception {
         //GIVEN
         userRepository.save(new User("1","esgoet", 6, goalDate, 0));
@@ -125,6 +131,7 @@ class UserControllerIntegrationTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser
     void deleteUserTest() throws Exception {
         //GIVEN
         userRepository.save(new User("1","esgoet", 6, goalDate, 0));
