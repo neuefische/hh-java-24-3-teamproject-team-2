5f254be90f928b7c5439e561cac9ab80dbe8f9a5
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-23 12:55:09 +0200

    Add styles for mobile viersion

--- frontend/src/pages/DashboardPage/dashboard/Dashboard.tsx
+++ frontend/src/pages/DashboardPage/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import "./Dashboard.css";
 import GreetingAndGoal from "../components/greeting/GoalAndSummary.tsx";
 
 import {Book, User} from "../../../types/types.ts";
@@ -11,10 +12,12 @@ type DashboardProps = {
 export default function Dashboard({user, data}: DashboardProps) {
 
     return (
-        <div id={"DashboardPage"}>
+        <div id={"dashboard-page"}>
             <h2 style={{paddingLeft: "15px"}}>Welcome to TaleTrail, <em>{user.userName}</em>!</h2>
-            <GreetingAndGoal user={user} data={data}/>
-            <LastAddedBook data={data}/>
+            <div>
+                <GreetingAndGoal user={user} data={data}/>
+                <LastAddedBook data={data}/>
+            </div>
         </div>
     );
 }
